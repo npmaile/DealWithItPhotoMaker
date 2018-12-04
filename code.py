dabc264e057c3b7409552014ceb39106c9599954
Nathaniel P Maile <npmaile@gmail.com>
2018-12-04 23:46:35 +0000

    moved some things around

--- code.py
+++ code.py
@@ -6,13 +6,18 @@ import sys
 import io
 import math
 from pprint import pprint
-os.chdir("/home/nate/Projects/GitHub/DealWithItPhotoMaker")
-glasses = Image.open("marker.png").convert("RGBA")#Brings those nasty Shadezzz
-Target = Image.open("barry obammy.jpg").convert("RGBA")#Brings in your victimage
-target4recognition = face_recognition.api.load_image_file("barry obammy.jpg")# loads target into facial recognition
-rawlocations = face_recognition.api.face_landmarks(target4recognition)
 
-def somefunction(bullshit):
+def ScrewUpThePhoto(target):
+    glasses = Image.open("marker.png").convert("RGBA")#Brings those nasty Shadezzz
+    opentarget = Image.open(target).convert("RGBA")#Brings in your victimage
+    target4recognition = face_recognition.api.load_image_file("barry obammy.jpg")# loads target into facial recognition
+    rawlocations = getRawEyeLocations(face_recognition.api.face_landmarks(target4recognition))
+    leftx = yolo[0]-64
+    lefty = yolo[1]-64
+    Target.paste(glasses,(leftx,lefty),glasses)
+    Target.show()
+    Target.save(testone)
+def getRawEyeLocations(facelandmarks):
     listone = []
     listtwo = []
     listthree = []
@@ -31,15 +36,10 @@ def somefunction(bullshit):
     lefty = int(sum(listtwo)/len(listtwo))
     rightx = int(sum(listthree)/len(listthree))
     righty = int (sum(listfour)/len(listfour))
-    toreturn = [leftx, lefty, rightx, righty]
-    return toreturn
-yolo = somefunction(rawlocations)#this function will return (lefteyex,lefteyey),(righteyex,righteyey)
-pprint(yolo)
-leftx = yolo[0]-64
-lefty = yolo[1]-64
-Target.paste(glasses,(leftx,lefty),glasses)
-Target.show()
-Target.save(testone)
+    ret = [leftx, lefty, rightx, righty]
+    return ret
+
+ScrewUpThePhoto("BarryObammy.jpg")
 #todo-- code to modify the glasses to be the correct size and orientation
 
 #Target.paste(glasses,(x,y),glasses)
